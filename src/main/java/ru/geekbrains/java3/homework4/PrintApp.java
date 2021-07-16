2760b0d8e4fa01c9515995efe00f9dd25cdca2f3
Garik-S3 <safronov-88@mail.ru>
2021-07-16 12:29:26 +0300

    Homework4

---
+++ src/main/java/ru/geekbrains/java3/homework4/PrintApp.java
@@ -0,0 +1,67 @@
+package ru.geekbrains.java3.homework4;
+
+public class PrintApp {
+
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+
+        PrintApp printApp = new PrintApp();
+
+        new Thread(() -> printApp.printA()).start();
+        new Thread(() -> printApp.printB()).start();
+        new Thread(() -> printApp.printC()).start();
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait();
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
