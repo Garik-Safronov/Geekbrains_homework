fd4b366cfa7172ee4e6e7d76f3b0a3b493166fd4
Garik-S3 <safronov-88@mail.ru>
2021-07-19 21:27:19 +0300

    Homework6 +Test

--- src/main/java/ru/geekbrains/java3/homework6/Homework6.java
+++ src/main/java/ru/geekbrains/java3/homework6/Homework6.java
@@ -2,34 +2,31 @@ package ru.geekbrains.java3.homework6;
 
 import java.util.Arrays;
 
-public class MainApp {
+public class Homework6 {
 
-    public static void main(String[] args) {
-
-        int[] arr = {2, 8, 2};
-        afterLastFour(arr);
-
-        int[] one = {1, 4, 1, 4};
-        System.out.println(oneAndFour(one));
-
-    }
-
-    public static int[] afterLastFour(int[] arr) {
+    public int[] afterLastFour(int[] arr) {
+        if (arr.length == 0){
+            throw new NullPointerException("Массив пустой");
+        }
         int index = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 4) {
                 index = i + 1;
             }
         }
+        if (index == 0) {
+            throw new RuntimeException("В массиве отсутствует 4");
+        }
         int[] newArr = new int[arr.length - index];
         System.arraycopy(arr, index, newArr, 0, newArr.length);
         System.out.println(Arrays.toString(newArr));
-        if (index == 0) {
-            throw new RuntimeException("В массиве отсутствует 4");
-        } else return newArr;
+        return newArr;
     }
 
-    public static boolean oneAndFour(int[] arr) {
+    public boolean oneAndFour(int[] arr) {
+        if (arr.length == 0){
+            throw new NullPointerException("Массив пустой");
+        }
         boolean one = false;
         boolean four = false;
         for (int value : arr) {
